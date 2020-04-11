7d4fa2772790e7b54dd6927f511433b797bc796b
Peter Taraba <taraba.peter@mail.com>
2020-04-10 17:26:16 -0700

    Cleaning up

---
+++ Optirol/Props/Settings.cs
@@ -0,0 +1,27 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Optirol.Properties.Settings
+// Assembly: Optirol, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 00BF1D7A-FB0A-4D90-A8E1-A8B009A65C83
+// Assembly location: C:\Users\tarab\OneDrive\Desktop\Optirol1.1\Optirol.exe
+
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace Optirol.Properties
+{
+  [GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
+  [CompilerGenerated]
+  internal sealed class Settings : ApplicationSettingsBase
+  {
+    private static Settings defaultInstance = (Settings) SettingsBase.Synchronized((SettingsBase) new Settings());
+
+    public static Settings Default
+    {
+      get
+      {
+        return Settings.defaultInstance;
+      }
+    }
+  }
+}
